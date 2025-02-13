5573b909ca0ede2628297c7d67b4867f4adf32be
DoanVietQuan <cocailinday105@gmail.com>
2025-02-13 15:03:08 +0700

    Update XinChao.java

--- HelloWorld/src/dvq/XinChao.java
+++ HelloWorld/src/dvq/XinChao.java
@@ -12,8 +12,9 @@ public class XinChao {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//
+		System.out.print("TÔI LẦN ĐẦU HỌC Java, và đang thử nghiệm quản lý mã nguồn với GIT");
+		System.out.print("Cập nhật lại chữ Java");
 	}
 
 }
