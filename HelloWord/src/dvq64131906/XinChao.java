4fcb5f370c62eda8155993ca84737f34e521436f
DoanVietQuan <cocailinday105@gmail.com>
2025-02-13 14:56:08 +0700

    Ví dụ đầu tiên
    Demo

---
+++ HelloWord/src/dvq64131906/XinChao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package dvq64131906;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//
+		System.out.print("TÔI LẦN ĐẦU HỌC JAVA, và đang thử nghiệm quản lý mã nguồn với GIT");
+	}
+
+}
