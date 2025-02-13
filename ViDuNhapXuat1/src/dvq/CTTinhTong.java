9396e1b51bd283d58a3a6e5aa1e53d4fd4068eaf
DoanVietQuan <cocailinday105@gmail.com>
2025-02-13 15:58:53 +0700

    Full code. Test OK

---
+++ ViDuNhapXuat1/src/dvq/CTTinhTong.java
@@ -0,0 +1,26 @@
+package dvq;
+
+import java.util.Scanner;
+
+public class CTTinhTong {
+
+	public static void main(String[] args) {
+		//In ra một dòng tiêu đề App
+		System.out.print("CT TINH TONG \n");
+		
+		//Chuẩn bị cho việc nhập
+		Scanner banPhim = new Scanner(System.in);
+		
+		//In dong huong nhan Moi nhap A
+		System.out.println("Mời nhập a: ");
+		double a= banPhim.nextDouble();
+		System.out.println("Mời nhập b: ");
+		double b= banPhim.nextDouble();
+		//Tính toán
+		double tong = a + b;
+		//
+		System.out.print("Tổng là: ");
+		System.out.print(tong);
+	}
+
+}
